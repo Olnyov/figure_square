df6c0e0b4f9153bad47d160beeeeaec505789c35
Pavel Olnyov <pavel@sebekon.ru>
2020-11-07 02:50:16 +0300

    Добавлен файл с константами

--- index.php
+++ index.php
@@ -8,23 +8,37 @@ define("APP_ENTRY", true);
 // Подключение простого автолоадера
 require_once 'simple_autoloader.php';
 
+// Подключение файла констант
+require_once 'constants.php';
+
 use App\Figure\FunctionPowX2;
 
 $message = '';
 
 try {
-    $x1 = !empty($_GET["x1"]) ? (float) $_GET["x1"] : 5;
-    $x2 = !empty($_GET["x2"]) ? (float) $_GET["x2"] : 25;
+    $x1 = !empty($_GET["x1"]) ? (float) $_GET["x1"] : DEFAULT_X1;
+    $x2 = !empty($_GET["x2"]) ? (float) $_GET["x2"] : DEFAULT_X2;
 
     // Since PHP 7.4
     foreach ([$x1, $x2] as $x){
-        if (filter_var($x, FILTER_VALIDATE_FLOAT, ["options" => ["min_range" => 0, "max_range"=> 10000]]) === false) {
+        $isValidParameter = filter_var(
+            $x,
+            FILTER_VALIDATE_FLOAT,
+            [
+                "options" => [
+                    "min_range" => VALIDATION_MIN_RANGE,
+                    "max_range"=> VALIDATION_MAX_RANGE
+                ]
+            ]
+        );
+
+        if ($isValidParameter === false) {
             throw new Exception("Не пройдена валидация входящих данных");
         }
     }
 
     $obj = new FunctionPowX2();
-    $square = round($obj->getSquare($x1, $x2), 4);
+    $square = round($obj->getSquare($x1, $x2), SQUARE_ROUND_PRECISION);
     $message = "Площадь криволинейной трапеции: $square";
 
 } catch (Throwable $error) {
@@ -34,6 +48,7 @@ try {
 <!DOCTYPE html>
 <html lang="ru">
     <head>
+        <title>Определение площади криволинейной трапеции</title>
         <meta charset="utf-8">
     </head>
     <body>
